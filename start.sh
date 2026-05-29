5ccaf4df46997372d7201b1167a8b29ee405c10e
JonimoK92 <jonathankmwela@gmail.com>
2026-05-30 01:40:42 +0200

    oui 12

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@
 echo "Starting Laravel..."
 echo "DB HOST = $DB_HOST"
 
-php artisan optimize:clear
 
+php artisan optimize:clear
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
