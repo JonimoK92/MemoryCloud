f3ce0ff964b00316d8c2c55062f532d50467b201
JonimoK92 <jonathankmwela@gmail.com>
2026-05-30 01:45:44 +0200

    oui 13

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 echo "Starting Laravel..."
-echo "DB HOST = $DB_HOST"
-
 
 php artisan optimize:clear
 php artisan config:clear
+
+chmod -R 777 storage bootstrap/cache
+
+php artisan migrate --force
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-php artisan migrate --force
+php artisan tinker --execute="dump(config('database.connections.pgsql.host'))"
 
 php-fpm -D
 nginx -g "daemon off;"
