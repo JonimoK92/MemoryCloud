93d9f01c2984c8ab9155ddc4d7b304be03ac7a4e
JonimoK92 <jonathankmwela@gmail.com>
2026-05-30 01:53:16 +0200

    oui 15

--- start.sh
+++ start.sh
@@ -5,15 +5,9 @@ echo "Starting Laravel..."
 php artisan optimize:clear
 rm -rf bootstrap/cache/*.php
 
-
 php artisan tinker --execute="dump(env('DB_HOST'));"
 
 php artisan migrate --force
-
-
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
 php-fpm -D
 nginx -g "daemon off;"
