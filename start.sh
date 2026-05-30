7bb3a9824dd5b732569bb4aac9fbe5997fabae19
JonimoK92 <jonathankmwela@gmail.com>
2026-05-30 02:30:12 +0200

    oui 20

--- start.sh
+++ start.sh
@@ -3,16 +3,9 @@
 echo "Starting MemoryCloud..."
 
 rm -rf bootstrap/cache/*.php
-chmod -R 775 storage bootstrap/cache
 
-echo "Waiting for DB..."
-
-sleep 5
-
+php artisan optimize:clear
 php artisan config:clear
-php artisan cache:clear
-
-php artisan config:cache
 
 php artisan migrate --force
 
